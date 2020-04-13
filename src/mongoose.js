77b8a36bd16b6a50e70d861deca585e6884ada3b
aman-codedocx <59098223+aman-codedocx@users.noreply.github.com>
2020-04-14 00:57:41 +0530

    updated db connection for redhat

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb:admin:redfort143@172.30.216.245:27017/sampledb', {
+mongoose.connect('mongodb://admin:redfort143@172.30.216.245:27017/sampledb', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useFindAndModify: false
